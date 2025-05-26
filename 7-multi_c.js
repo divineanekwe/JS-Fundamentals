ae351f63458b308c525817d6894c39103b53c351
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2025-05-26 14:06:37 +0100

    fix: 7-multi_c.js - Adjust loop syntax and handle positive occurrences

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,10 +2,10 @@ const { argv } = require('node:process');
 
 let firstArgument = parseInt(argv[2]);
 
-if(Number.isNaN(firstArgument)){
+if (Number.isNaN(firstArgument)){
 	console.log('Missing number of occurrences');
-} else{
-	for(let i = 0; i < firstArgument; i++){
+} else if (firstArgument > 0){
+	for (let i = 0; i < firstArgument; i++){
 		console.log('C is fun');
 	}
 }
