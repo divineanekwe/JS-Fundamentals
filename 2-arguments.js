811071d380b82addfc81659323c29e4abf03fc20
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2025-05-26 08:38:32 +0100

    refactor: 2-arguments.js - Simplify argument counting logic

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,8 @@
 const { argv } = require('node:process');
 
-count = 0;
-argv.forEach((val, index) => {
-	count++;
-});
-
-if(count < 3){
+if(argv.length < 3){
 	console.log('No argument');
-} else if(count == 3){
+} else if(argv.length == 3){
 	console.log('Argument found');
 } else{
 	console.log('Arguments found');
