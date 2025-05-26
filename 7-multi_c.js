1b177eaf7d429c0f47ef3f39fd31e3bb670d509e
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2025-05-26 13:58:20 +0100

    feat: 7-multi_c.js - Prints 'C is fun' multiple times

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const { argv } = require('node:process');
+
+let firstArgument = parseInt(argv[2]);
+
+if(Number.isNaN(firstArgument)){
+	console.log('Missing number of occurrences');
+} else{
+	for(let i = 0; i < firstArgument; i++){
+		console.log('C is fun');
+	}
+}
